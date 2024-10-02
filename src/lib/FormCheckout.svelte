794c5e5041548f3f5f186009a629678247b6fad3
tdy81 <155242274+tdy81@users.noreply.github.com>
2024-10-02 11:18:34 +0200

    Update settings

--- src/lib/FormCheckout.svelte
+++ src/lib/FormCheckout.svelte
@@ -1,5 +1,7 @@
 <script>
-    import html2pdf from 'html2pdf.js'   
+    import { onMount } from 'svelte';
+    import html2pdf from 'html2pdf.js'; 
+
     export let currentTab;
 
     function tabShow(el) {
@@ -7,25 +9,28 @@
     }
 
     async function handlePrint() {
-    const element = document.getElementById('wrap-preview');
+        const element = document.getElementById('wrap-preview');
 
-    var opt = {
-        margin: 0,
-        filename: 'cv.pdf',
-        image: { type: 'jpeg', quality: 1 },
-        html2canvas: { scale: 4 }
-    };
-    html2pdf().set(opt).from(element).save();
-    }
+        var opt = {
+            margin: 0,
+            filename: 'cv.pdf',
+            image: { type: 'jpeg', quality: 1 },
+            html2canvas: { scale: 4 }
+        };
+        html2pdf().set(opt).from(element).save();
+    }  
+
+    onMount(async () => {
+        paypal.HostedButtons({
+            hostedButtonId: "W6ZSFJ4RGR3SU",
+        }).render("#paypal-container-W6ZSFJ4RGR3SU")
+    });
 </script>
 
 <div class="text-xl text-black font-bold mb-4">Checkout</div>
 
 <div class="border rounded-lg p-6 mb-6">
-    <div class="flex justify-between mb-4">
-        <div>CV.pdf</div>
-        <div class="font-bold">1€</div>
-    </div>
-    <button type="button" class="w-full mb-4 px-6 py-3 uppercase text-xs bg-white text-black border border-black hover:bg-black hover:border-black hover:text-white rounded">Button stripe</button>
+    <div class="mb-4">Vous pouvez faire un don pour soutenir le projet.</div>
+    <div id="paypal-container-W6ZSFJ4RGR3SU"></div>
     <button type="button" class="w-full px-6 py-3 uppercase text-xs bg-black text-white border border-black hover:bg-white hover:border-black hover:text-black rounded" on:click={handlePrint}>Download PDF</button>
 </div>
